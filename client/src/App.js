48cfb1bea0d2b4580b5185b6ac309c0af21e9114
Felipe Lujan <jfelipelujan@gmail.com>
2018-07-06 23:51:05 -0500

    Router and Routes installed, first routes implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Components
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact component={Register} path="/register" />
+            <Route exact component={Login} path="/login" />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
