9755641d492ecf95423d627234bbae45763fc547
pipe <jfelipelujan@gmail.com>
2018-07-10 23:58:14 -0500

    Capital U

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -3,7 +3,7 @@ import { TEST_DISPATCH } from "./types";
 
 //this is an action creator
 //This will dispatch userdata to the reducer that takes the type TEST_DISPATCH
-export const registeruser = userData => {
+export const registerUser = userData => {
   return {
     type: TEST_DISPATCH,
     payload: userData
