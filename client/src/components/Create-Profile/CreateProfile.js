327797e1a4d9b4df6f8ff94e3ee9c23691a63813
FelipeLujan <jfelipelujan@gmailcom>
2018-07-20 22:54:54 -0500

    create profile component initialized

---
+++ client/src/components/Create-Profile/CreateProfile.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class CreateProfile extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+function mapStateToProps(state) {
+  return {};
+}
+
+export default connect(mapStateToProps)(CreateProfile);
