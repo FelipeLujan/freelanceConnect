6a5b95b118200dacbb4dc21b7eb6caff9712721b
FelipeLujan <jfelipelujan@gmail.com>
2018-07-24 00:09:14 -0500

    Revert: Delete keys.js

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+keys = {
+  mongoURI: "mongodb://felipe:hanafuda@ds031597.mlab.com:31597/devconnector",
+  secretOrKey:
+    "er pastrami capicola porchetta. Short loin ham brisket, sausage sirloin jerky venison flank ground round h"
+};
+/*mlab URI user and pass*/
+module.exports = keys;
